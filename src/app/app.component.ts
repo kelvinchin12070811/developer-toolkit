1409006a30b0c0b1057d5874167e88d46129db7f
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-16 00:38:41 +0800

    Make sidebar highlight on selected menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
 })
 export class AppComponent {
-  router = inject(Router);
-
   title = 'developer-toolkit';
-
-  OnInit() {
-    console.log(this.router.url);
-  }
 }
