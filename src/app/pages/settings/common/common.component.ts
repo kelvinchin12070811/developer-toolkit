132774bd86879ed090a6f3096f7773e637ec8b81
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-17 01:38:14 +0800

    Experimented with theme changer

--- src/app/pages/settings/common/common.component.ts
+++ src/app/pages/settings/common/common.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-common',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './common.component.html',
-  styleUrls: ['./common.component.scss']
+  styleUrls: ['./common.component.scss'],
 })
 export class CommonComponent {
+  theme: string = localStorage.getItem('theme') ?? 'light';
 
+  constructor() {
+    this.setTheme();
+  }
+
+  onThemeChanged() {
+    this.setTheme();
+    localStorage.setItem('theme', this.theme);
+  }
+
+  private setTheme() {
+    document.querySelector('html')?.setAttribute('data-theme', this.theme);
+  }
 }
