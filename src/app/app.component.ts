e7b7aad3f6406da7f2927be35cea28b1eb519973
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-16 23:28:57 +0800

    Separated sidebar and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, SidebarComponent, HeaderComponent],
 })
 export class AppComponent {
   titleService = inject(Title);
