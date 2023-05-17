b2088f69db956b58552d57ab3bf791c1cdda67bb
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-18 00:47:25 +0800

    Added UUID Generator

--- src/app/pages/generators/uuid/uuid.component.ts
+++ src/app/pages/generators/uuid/uuid.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { v4 as uuidv4 } from 'uuid';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-uuid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './uuid.component.html',
-  styleUrls: ['./uuid.component.scss']
+  styleUrls: ['./uuid.component.scss'],
 })
 export class UuidComponent {
+  uuid = signal<string>('');
+  uppercased = signal<boolean>(false);
 
+  genUUID() {
+    this.uuid.set(uuidv4());
+    console.log(this.uuid());
+  }
+
+  getUUID() {
+    return this.uppercased() ? this.uuid().toUpperCase() : this.uuid();
+  }
+
+  getGUID() {
+    const uuid = this.getUUID();
+    return uuid === '' ? '' : `{${uuid}}`;
+  }
 }
