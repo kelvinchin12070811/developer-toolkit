35e7fb0525908063d616965316c5e7a69b52e189
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-18 01:20:05 +0800

    Added timestamp generator

--- src/app/pages/generators/timestamp/timestamp.component.ts
+++ src/app/pages/generators/timestamp/timestamp.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as dayjs from 'dayjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-timestamp',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './timestamp.component.html',
-  styleUrls: ['./timestamp.component.scss']
+  styleUrls: ['./timestamp.component.scss'],
 })
 export class TimestampComponent {
+  timestamp = signal('');
+  type = signal('iso8601');
 
+  ngOnInit() {
+    this.genISO860Timestamp();
+  }
+
+  generateTimestamp() {
+    switch (this.type()) {
+      case 'iso8601':
+        this.genISO860Timestamp();
+        break;
+      case 'iso8601-no-separator':
+        this.genISO8601NoSepTimestamp();
+        break;
+      case 'unix':
+        this.genUnixTimestemp();
+        break;
+      default:
+        this.timestamp.set('');
+    }
+  }
+
+  genISO860Timestamp() {
+    const date = dayjs();
+    this.timestamp.set(date.format('YYYY-MM-DDTHH:mm:ss'));
+  }
+
+  genISO8601NoSepTimestamp() {
+    const date = dayjs();
+    this.timestamp.set(date.format('YYYYMMDDTHHmmss'));
+  }
+
+  genUnixTimestemp() {
+    const data = dayjs();
+    this.timestamp.set(data.unix().toString());
+  }
 }
