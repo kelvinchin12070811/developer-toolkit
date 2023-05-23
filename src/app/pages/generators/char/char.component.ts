4fa34c3e87c6bef7431fa2e06a157cf5a2681e53
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-24 01:15:27 +0800

    Completed function of the character generator

--- src/app/pages/generators/char/char.component.ts
+++ src/app/pages/generators/char/char.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+const symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 @Component({
   selector: 'app-char',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './char.component.html',
-  styleUrls: ['./char.component.scss']
+  styleUrls: ['./char.component.scss'],
 })
 export class CharComponent {
+  genUpperCase = signal(true);
+  genLowerCase = signal(false);
+  genNumbers = signal(false);
+  genSymbols = signal(false);
+  length = signal(10);
+  result = signal('');
 
+  generate() {
+    let newResult = '';
+
+    for (let times = 0; times < this.length(); times++) {
+      let category: number[] = [];
+
+      if (this.genLowerCase()) category.push(1);
+      if (this.genUpperCase()) category.push(0);
+      if (this.genNumbers()) category.push(2);
+      if (this.genSymbols()) category.push(3);
+
+      switch (category[Math.floor(Math.random() * category.length)]) {
+        case 0:
+          newResult += this.uppercaseGenerator();
+          break;
+        case 1:
+          newResult += this.lowercaseGenerator();
+          break;
+        case 2:
+          newResult += this.numberGenerator();
+          break;
+        case 3:
+          newResult += this.symbolGenerator();
+          break;
+      }
+    }
+
+    this.result.set(newResult);
+  }
+
+  uppercaseGenerator() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+
+  lowercaseGenerator() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+
+  numberGenerator() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+
+  symbolGenerator() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
 }
