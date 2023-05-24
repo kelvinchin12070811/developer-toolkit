b958c8d30d0fc9ae0127bc72ffb15aee9c4f44b9
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-25 02:13:37 +0800

    Completed function of generating lorem ipsum

--- src/app/pages/generators/lorem-ipsum/lorem-ipsum.component.ts
+++ src/app/pages/generators/lorem-ipsum/lorem-ipsum.component.ts
@@ -1,15 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
-import { FormControl } from '@angular/forms';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { LoremIpsum } from 'lorem-ipsum';
 
 @Component({
-  selector: 'app-lorem-ipsum',
-  standalone: true,
-  imports: [CommonModule, GeneratorCardComponent],
-  templateUrl: './lorem-ipsum.component.html',
-  styleUrls: ['./lorem-ipsum.component.scss'],
+    selector: 'app-lorem-ipsum',
+    standalone: true,
+    imports: [CommonModule, GeneratorCardComponent, ReactiveFormsModule, FormsModule],
+    templateUrl: './lorem-ipsum.component.html',
+    styleUrls: ['./lorem-ipsum.component.scss'],
 })
 export class LoremIpsumComponent {
-  form = new FormControl();
+    form = new FormGroup({
+        size: new FormControl(3, [Validators.required, Validators.min(1)]),
+        type: new FormControl('paragraph'),
+    });
+    lorem = new LoremIpsum();
+    result = signal('');
+
+    get sizeField() {
+        return this.form.get('size');
+    }
+
+    print() {
+        if (!this.form.valid) return;
+
+        const values = this.form.value;
+        console.log(typeof values.size);
+        console.log(values.type);
+
+        switch (values.type) {
+            case 'sentence':
+                this.result.set(
+                    this.lorem.generateSentences(values?.size ?? 0).replaceAll('\n', '\n\n')
+                );
+                break;
+            case 'word':
+                this.result.set(
+                    this.lorem.generateWords(values?.size ?? 3).replaceAll('\n', '\n\n')
+                );
+                break;
+            case 'paragraph':
+                this.result.set(
+                    this.lorem.generateParagraphs(values?.size ?? 0).replaceAll('\n', '\n\n')
+                );
+                break;
+        }
+    }
 }
