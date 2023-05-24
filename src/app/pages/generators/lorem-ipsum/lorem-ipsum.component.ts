72907c59a8b92585c5e9fbc6e0439938334cf5df
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-25 01:17:37 +0800

    Completed form ui for lorem ipsum gen

--- src/app/pages/generators/lorem-ipsum/lorem-ipsum.component.ts
+++ src/app/pages/generators/lorem-ipsum/lorem-ipsum.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-lorem-ipsum',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GeneratorCardComponent],
   templateUrl: './lorem-ipsum.component.html',
-  styleUrls: ['./lorem-ipsum.component.scss']
+  styleUrls: ['./lorem-ipsum.component.scss'],
 })
 export class LoremIpsumComponent {
-
+  form = new FormControl();
 }
