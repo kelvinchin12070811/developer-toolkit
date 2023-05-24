15b06742eb4359674f412db1622737043c25474d
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-25 00:20:42 +0800

    Migrated timestamp generator to use GeneratorComponent

--- src/app/pages/generators/timestamp/timestamp.component.ts
+++ src/app/pages/generators/timestamp/timestamp.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import * as dayjs from 'dayjs';
 import { FormsModule } from '@angular/forms';
 import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
+import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
 
 @Component({
   selector: 'app-timestamp',
   standalone: true,
-  imports: [CommonModule, FormsModule, ContainerCardComponent],
+  imports: [CommonModule, FormsModule, GeneratorCardComponent],
   templateUrl: './timestamp.component.html',
   styleUrls: ['./timestamp.component.scss'],
 })
