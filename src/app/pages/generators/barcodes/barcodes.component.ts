a4639c93e5cf33255324616c949cf9afa354ba8b
Kelvin Chin <chinkaiwen@yahoo.com>
2023-06-01 01:16:14 +0800

    Added UI for barcode generator

--- src/app/pages/generators/barcodes/barcodes.component.ts
+++ src/app/pages/generators/barcodes/barcodes.component.ts
@@ -2,22 +2,26 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // @ts-ignore
 import bwipjs from 'bwip-js';
+import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-barcodes',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, GeneratorCardComponent, FormsModule],
     templateUrl: './barcodes.component.html',
     styleUrls: ['./barcodes.component.scss'],
 })
 export class BarcodesComponent {
     barcode = signal<string>('');
+    text = signal<string>('');
+    barcodeType = signal<string>('datamatrix');
 
-    ngOnInit() {
+    generateBarcode() {
         let canvas = document.createElement('canvas');
         bwipjs.toCanvas(canvas, {
             bcid: 'datamatrix',
-            text: '0123456789',
+            text: this.text(),
             scale: 10,
         });
         this.barcode.set(canvas.toDataURL());
