199deed2e90851e7a67cb2cd77e278b0c4178d7a
Kelvin Chin <chinkaiwen@yahoo.com>
2023-06-14 01:55:57 +0800

    Added link shortener page

---
+++ src/app/pages/utils/link-shortener/link-shortener.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-link-shortener',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './link-shortener.component.html',
+  styleUrls: ['./link-shortener.component.scss']
+})
+export class LinkShortenerComponent {
+
+}
