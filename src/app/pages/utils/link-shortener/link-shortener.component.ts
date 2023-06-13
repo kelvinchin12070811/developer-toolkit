d1b22e627a50930b17adda7f02869455034dbea3
Kelvin Chin <chinkaiwen@yahoo.com>
2023-06-14 02:09:41 +0800

    Added UI for link shortener

--- src/app/pages/utils/link-shortener/link-shortener.component.ts
+++ src/app/pages/utils/link-shortener/link-shortener.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
 
 @Component({
-  selector: 'app-link-shortener',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './link-shortener.component.html',
-  styleUrls: ['./link-shortener.component.scss']
+    selector: 'app-link-shortener',
+    standalone: true,
+    imports: [CommonModule, ContainerCardComponent],
+    templateUrl: './link-shortener.component.html',
+    styleUrls: ['./link-shortener.component.scss'],
 })
-export class LinkShortenerComponent {
-
-}
+export class LinkShortenerComponent {}
