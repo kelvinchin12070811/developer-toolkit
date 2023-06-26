d5494175d49b91e1b4a542fe207a068345ff8b9e
Kelvin Chin <chinkaiwen@yahoo.com>
2023-06-26 22:29:48 +0800

    Scrapped project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './common/sidebar/sidebar.component';
-import { HeaderComponent } from './common/header/header.component';
-import { ConfigService } from './services/config.service';
-import { FooterComponent } from './common/footer/footer.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [RouterOutlet, RouterModule, SidebarComponent, HeaderComponent, FooterComponent],
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    titleService = inject(Title);
-    configService = inject(ConfigService);
-
-    title = 'developer-toolkit';
-
-    constructor() {
-        this.titleService.setTitle('Developer Toolkit');
-    }
-
-    ngOnInit() {
-        this.configService.setTheme();
-    }
+  title = 'developer-toolkit';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { ActivatedRoute, provideRouter } from '@angular/router';
-import { AppRoutingModule, routes } from './app-routing.module';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideClientHydration(), AppRoutingModule],
+  providers: [provideRouter(routes) ]
 };
