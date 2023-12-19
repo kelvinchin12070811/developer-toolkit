f82d8c9ecd1965c239ac42a2c0780c87db703558
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-19 15:52:48 +0800

    Added string hash utilities category

---
+++ src/app/pages/string-hash/string-hash.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StringHashRoutingModule } from './string-hash-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StringHashRoutingModule
+  ]
+})
+export class StringHashModule { }
