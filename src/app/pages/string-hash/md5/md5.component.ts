71d3bccafec0852ad6f56a2515f2545515ec5f3b
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-19 16:15:30 +0800

    Added link to md5 hasher in sidebar

--- src/app/pages/string-hash/md5/md5.component.ts
+++ src/app/pages/string-hash/md5/md5.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
 
 @Component({
-  selector: 'app-md5',
-  standalone: true,
-  imports: [],
-  templateUrl: './md5.component.html',
-  styleUrl: './md5.component.scss'
+    selector: 'app-md5',
+    standalone: true,
+    imports: [ContainerCardComponent],
+    templateUrl: './md5.component.html',
+    styleUrl: './md5.component.scss',
 })
-export class Md5Component {
-
-}
+export class Md5Component {}
