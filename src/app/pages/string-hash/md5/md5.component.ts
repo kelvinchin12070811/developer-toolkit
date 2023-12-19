88eb473a9fb7ae7e2356b0fe7f55df5a52327606
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-19 16:34:34 +0800

    completed basic md5 hasher function

--- src/app/pages/string-hash/md5/md5.component.ts
+++ src/app/pages/string-hash/md5/md5.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
+import { Component, effect, signal } from '@angular/core';
+import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
+import { MD5, enc } from 'crypto-js';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-md5',
     standalone: true,
-    imports: [ContainerCardComponent],
+    imports: [GeneratorCardComponent, FormsModule],
     templateUrl: './md5.component.html',
     styleUrl: './md5.component.scss',
 })
-export class Md5Component {}
+export class Md5Component {
+    public text = signal('');
+    public hash = signal('');
+
+    onGenerate() {
+        this.hash.set(MD5(this.text()).toString(enc.Hex));
+    }
+}
