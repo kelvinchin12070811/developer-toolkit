19d5d5a8efaa53a1a51a5b4f6c24bd00a60a9454
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-20 23:51:10 +0800

    Added Services Worker for pwa

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ************************************************************************************************************/
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(
-            BrowserModule,
-            AppRoutingModule,
-            HttpClientModule,
-            HttpClientJsonpModule
-        ),
-    ],
+    importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule, HttpClientJsonpModule),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 }).catch(err => console.error(err));
