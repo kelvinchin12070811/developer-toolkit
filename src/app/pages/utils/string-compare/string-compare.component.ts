d6d40c2a1827c4b48078f8a4335799a91c990b4b
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-20 11:38:15 +0800

    Added visualizer for not matched string

--- src/app/pages/utils/string-compare/string-compare.component.ts
+++ src/app/pages/utils/string-compare/string-compare.component.ts
@@ -2,6 +2,11 @@ import { Component, HostListener, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
 
+type MissMatchPattern = {
+    match: string;
+    unmatch: string;
+};
+
 @Component({
     selector: 'app-string-compare',
     standalone: true,
@@ -10,18 +15,29 @@ import { ContainerCardComponent } from 'src/app/common/container-card/container-
     styleUrl: './string-compare.component.scss',
 })
 export class StringCompareComponent {
-    public stringMatch = signal<boolean | null>(null);
+    public stringMatch = signal<number | null>(null);
     public leftHandSide = signal('');
     public rightHandSide = signal('');
+    public missMatchPattern = signal<MissMatchPattern>({ match: '', unmatch: '' });
 
     public getResult() {
-        if (this.leftHandSide() == '' && this.rightHandSide() == '') return;
-
-        this.stringMatch.set(this.leftHandSide().match(this.rightHandSide()) != null);
+        this.compareStrings();
+        this.missMatchPattern.set({
+            match: this.getMatchedCharacters(),
+            unmatch: this.getUnmatchedCharacters(),
+        });
     }
 
     public shouldCompareBtnDisabled() {
         return this.leftHandSide() == '' || this.rightHandSide() == '';
+    }
+
+    public getMatchedCharacters() {
+        return this.rightHandSide().substring(0, this.stringMatch()! - 1);
+    }
+
+    public getUnmatchedCharacters() {
+        return this.rightHandSide().substring(this.stringMatch()! - 1);
     }
 
     @HostListener('document:keypress', ['$event'])
@@ -35,5 +51,27 @@ export class StringCompareComponent {
         this.stringMatch.set(null);
         this.leftHandSide.set('');
         this.rightHandSide.set('');
+    }
+
+    private compareStrings() {
+        if (this.leftHandSide() == '' && this.rightHandSide() == '') return;
+
+        if (this.leftHandSide() === this.rightHandSide()) {
+            this.stringMatch.set(0);
+            return;
+        }
+
+        for (
+            let i = 0;
+            i < Math.min(this.leftHandSide().length, this.rightHandSide().length);
+            i++
+        ) {
+            if (this.leftHandSide()[i] !== this.rightHandSide()[i]) {
+                this.stringMatch.set(i + 1);
+                return;
+            }
+        }
+
+        this.stringMatch.set(Math.min(this.leftHandSide().length, this.rightHandSide().length) + 1);
     }
 }
