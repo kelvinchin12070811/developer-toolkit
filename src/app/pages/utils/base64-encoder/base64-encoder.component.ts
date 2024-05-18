dd8666f30dfd491e6c902d61eb077e728b8f9f10
Kelvin <chinkaiwen_35@outlook.com>
2024-05-18 16:56:41 +0000

    Completed base64 encoder

--- src/app/pages/utils/base64-encoder/base64-encoder.component.ts
+++ src/app/pages/utils/base64-encoder/base64-encoder.component.ts
@@ -4,23 +4,23 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ************************************************************************************************************/
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ContainerCardComponent } from 'src/app/common/container-card/container-card.component';
 import { GeneratorCardComponent } from 'src/app/common/generator-card/generator-card.component';
 
 @Component({
-  selector: 'app-base64-encoder',
-  standalone: true,
-  imports: [CommonModule, ContainerCardComponent, GeneratorCardComponent],
-  templateUrl: './base64-encoder.component.html',
-  styleUrl: './base64-encoder.component.scss'
+    selector: 'app-base64-encoder',
+    standalone: true,
+    imports: [CommonModule, FormsModule, ContainerCardComponent, GeneratorCardComponent],
+    templateUrl: './base64-encoder.component.html',
+    styleUrl: './base64-encoder.component.scss',
 })
 export class Base64EncoderComponent {
-  public noCopy: boolean = false;
-  public disableGeneratorButton: boolean = false;
+    base64String = signal<string>('');
+    input = signal<string>('');
 
-  onGenerate(): void {
-  }
-
-  onCopy(): void {}
+    onGenerate(): void {
+        this.base64String.set(btoa(this.input()));
+    }
 }
