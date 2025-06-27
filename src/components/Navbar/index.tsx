d93431b71da492e54da4554a23d263fda96d324d
Kelvin Chin <chinkaiwen_35@outlook.com>
2025-06-27 17:14:02 +0800

    Enhance Developer Toolkit label for improved styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,9 @@ export function Navbar() {
         <label htmlFor='main-drawer' className='drawer-button btn btn-ghost btn-square lg:hidden'>
           <MdMenu size={24} />
         </label>
-        <a className='btn btn-ghost text-xl font-bold p-1'>/://Developer Toolkit</a>
+        <a className='btn btn-ghost text-xl font-bold p-1'>
+          <span className='-mt-0.5 -mr-1.5'>/://</span>Developer Toolkit
+        </a>
       </div>
       <div className='flex gap-2'>
         <div className='tooltip tooltip-left' data-tip='GitHub Repository'>
